787b0a0e71b95aaaa039cfbceefdf2a220274888
irina.mamurkova <IrinaMamurkova1506@yandex.ru>
2026-06-16 20:39:11 +0800

    Реализована обработка запросов ввода-вывода

--- task_3/3.cpp
+++ task_3/3.cpp
@@ -30,5 +30,20 @@ int main() {
   int num_requests = 0;
   std::cin >> num_streets >> num_requests;
   SnowTracker tracker(num_streets);
+  for(int i = 0; i < num_requests; ++i) {
+    int type = 0;
+    std::cin >> type;
+    if (type == 1) {
+      int street_id = 0;
+      int64_t snow = 0;
+      std::cin >> street_id >> snow;
+      tracker.AddSnow(street_id, snow);
+    } else if (type == 2) {
+      int start_id = 0;
+      int end_id = 0;
+      std::cin >> start_id >> end_id;
+      std::cout << tracker.TotalSnow(start_id, end_id) << std::endl;
+    }
+  }
   return 0;
 }
