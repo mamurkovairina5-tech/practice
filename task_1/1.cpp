84925f13621f9072658e093ea722dbf6019afe93
irina.mamurkova <IrinaMamurkova1506@yandex.ru>
2026-06-16 16:11:11 +0800

    Реализован послойный расчёт времени спуска сверху вниз в dp

--- task_1/1.cpp
+++ task_1/1.cpp
@@ -12,6 +12,32 @@ void FindShortestPath(std::vector<std::vector<int>>& pyramid, int n) {
     parent[row].resize(row + 1, {-1, -1});
   }
   dp[0][0] = pyramid[0][0];
+
+  // Послойный расчет минимального времени спуска сверху вниз
+  for (int row = 0; row < n - 1; ++row) {
+    for (int column = 0; column <= row; ++column) {
+      if (dp[row][column] == INF) {
+        continue;
+      }
+      int next_row = row + 1;
+
+      //Шаг вниз (в ту же колонку column)
+      int next_column = column;
+      int time_column = dp[row][column] + pyramid[next_row][next_column];
+      if (time_column < dp[next_row][next_column]) {
+        dp[next_row][next_column] = time_column;
+        parent[next_row][next_column] = {row, column};
+      }
+
+      // Шаг вниз-направо (в колонку column + 1)
+      int next_column_right = column + 1;
+      int time_column_right = dp[row][column] + pyramid[next_row][next_column_right];
+      if (time_column_right < dp[next_row][next_column_right]) {
+        dp[next_row][next_column_right] = time_column_right;
+        parent[next_row][next_column_right] = {row, column};
+      }
+    }
+  }
 }
 
 int main() {
