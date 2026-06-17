b8fd29ad8bdf85e197d85325b4367e4c8329b5ec
irina.mamurkova <IrinaMamurkova1506@yandex.ru>
2026-06-17 16:46:36 +0800

    Реализован алгоритм поиска координат ограничивающего прямоугольника

--- task_5/5.cpp
+++ task_5/5.cpp
@@ -11,5 +11,28 @@ int main() {
       std::cin >> matrix[row][column];
     }
   }
+  int min_column = width;
+  int max_column = -1;
+  int min_row = height;
+  int max_row = -1;
+  for (int row = 0; row < height; ++row) {
+    for (int column = 0; column < width; ++column) {
+      if (matrix[row][column] == 1) {
+        if (row < min_row) {
+          min_row = row;
+        }
+        if (row > max_row) {
+          max_row = row;
+        }
+        if (column < min_column) {
+          min_column = column;
+        }
+        if (column > max_column) {
+          max_column = column;
+        }
+      }
+    }
+  }
+  std::cout << min_row - 1 << " " << min_column - 1 << " " << max_row + 1 << " " << max_column + 1 << std::endl;
   return 0;
 }
