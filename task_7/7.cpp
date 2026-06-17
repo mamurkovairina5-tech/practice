44aa6fd75508c69891f7e6b508487446ae7d7387
irina.mamurkova <IrinaMamurkova1506@yandex.ru>
2026-06-17 18:12:30 +0800

    Реализована функция проверки на правильный автомобильный номер

--- task_7/7.cpp
+++ task_7/7.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <string>
 
-bool IsVehicleNumber(std::string& number) {
-  return false;
+bool IsVehicleNumber(const std::string& number) {
+  if (number.length() != 6) {
+    return false;
+  }
+  if (number[0] < 'A' || number[0] > 'Z') {
+    return false;
+  }
+  if (number[1] < '0' || number[1] > '9') {
+    return false;
+  }
+  if (number[2] < '0' || number[2] > '9') {
+    return false;
+  }
+  if (number[3] < '0' || number[3] > '9') {
+    return false;
+  }
+  if (number[4] < 'A' || number[4] > 'Z') {
+    return false;
+  }
+  if (number[5] < 'A' || number[5] > 'Z') {
+    return false;
+  }
+  return true;
 }
 
 int main() {
